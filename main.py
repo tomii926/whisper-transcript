7f01e3c6e51b996f76868a338ef5c29d7e1a641b
Eisuke15 <hideyu41691@gmail.com>
2023-04-10 04:29:19 +0900

    refactoring

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-from pyannote.audio import Pipeline
 import os
 
+import whisper
+from pyannote.audio import Audio, Pipeline
+
+
 token = os.environ["HUGGINGFACE_API_KEY"]
+audio_file = 'sample.wav'
 
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1", use_auth_token=token)
-diarization = pipeline("sample.wav")
+diarization = pipeline(audio_file, num_speakers=3)
 
-for turn, _, speaker in diarization.itertracks(yield_label=True):
-    print(f"start={turn.start:.1f}s stop={turn.end:.1f}s speaker_{speaker}")
-
-
-import whisper
 model = whisper.load_model("large")
-
-from pyannote.audio import Audio
-
-audio_file = "audio.wav"
-diarization = pipeline(audio_file)
 
 audio = Audio(sample_rate=16000, mono=True)
 
-for segment, _, speaker in diarization.itertracks(yield_label=True):
-    waveform, sample_rate = audio.crop(audio_file, segment)
-    text = model.transcribe(waveform.squeeze().numpy())["text"]
-    print(f"[{segment.start:03.1f}s - {segment.end:03.1f}s] {speaker}: {text}")
+with open('result.txt', 'w') as f:
+    for segment, _, speaker in diarization.itertracks(yield_label=True):
+        waveform, sample_rate = audio.crop(audio_file, segment)
+        text = model.transcribe(waveform.squeeze().numpy())["text"]
+        log = f"[{segment.start:03.1f}s - {segment.end:03.1f}s] {speaker}: {text}"
+        print(log, file=f)
+        print(log)
