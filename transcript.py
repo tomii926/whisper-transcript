0fb2c303cb0a9c98632e2e6dff23980aefff2f70
Eisuke15 <hideyu41691@gmail.com>
2023-04-22 00:57:17 +0900

    change extension

--- transcript.py
+++ transcript.py
@@ -1,7 +1,7 @@
 import whisper
 
 model = whisper.load_model("large-v2")
-result = model.transcribe("sample.wav")
+result = model.transcribe("sample.m4a")
 print(result["text"])
 
 with open('transcript-v2.txt', 'w') as f:
