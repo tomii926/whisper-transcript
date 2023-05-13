fa9397f240accc3c6385b3bd01c30ca3517b71af
tomii926 <hideyu41691@gmail.com>
2023-05-14 04:19:01 +0900

    QA with langchain

--- qa.py
+++ qa.py
@@ -1,17 +1,12 @@
 from dotenv import load_dotenv
-
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 
-
 load_dotenv()
 
-loader = TextLoader('transcript-v2.txt')
-
+loader = TextLoader('transcript.txt')
 index = VectorstoreIndexCreator().from_loaders([loader])
+query = "話者の名前は？"
 
-query = """
-話者の名前は？
-"""
-print(index.query(query, chain_type="map_reduce"))
+print(index.query(query, chain_type="map_reduce", verbose=True))
 
