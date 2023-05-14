065e36ad9b589eac3721bc013fe9b4330a35ea4d
tomii926 <hideyu41691@gmail.com>
2023-05-14 19:13:14 +0900

    loop questions

--- qa.py
+++ qa.py
@@ -6,7 +6,13 @@ load_dotenv()
 
 loader = TextLoader('transcript.txt')
 index = VectorstoreIndexCreator().from_loaders([loader])
-query = "話者の名前は？"
 
-print(index.query(query, chain_type="map_reduce", verbose=True))
+with open('questions.txt', 'r') as f:
+    queries = f.readlines()
+    for query in queries:
+        query = query.strip()
+        print(query)
+        query += '日本語で答えてください。'
+        print(index.query(query, chain_type="map_reduce"))
+        print('')
 
